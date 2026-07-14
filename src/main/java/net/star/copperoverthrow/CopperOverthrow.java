59e8f866516ebb515bd8f4e959edc0aaf8f09cd3
fyodor-starovoytov <fyodor.starovoytov@gmail.com>
2026-07-14 21:06:21 +0300

    add first item

--- src/main/java/net/star/copperoverthrow/CopperOverthrow.java
+++ src/main/java/net/star/copperoverthrow/CopperOverthrow.java
@@ -1,5 +1,6 @@
 package net.star.copperoverthrow;
 
+import net.star.copperoverthrow.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -50,6 +51,15 @@ public class CopperOverthrow {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+
+
+
+        ModItems.register(modEventBus);
+
+
+
+
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -64,6 +74,10 @@ public class CopperOverthrow {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
+        /*Adds to the INGRIDIENTS creative tab*/
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.COPPER_NUGGET);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/star/copperoverthrow/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.star.copperoverthrow.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.star.copperoverthrow.CopperOverthrow;
+
+public class ModItems {
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(CopperOverthrow.MOD_ID);
+    //Registering the Items into Neoforge, long list of all registries
+    // "(CopperOverthrow.MOD_ID)" is telling under which MOD_ID Minecraft has to add the registries
+
+    //Adding the item to the game without any properties YET, just adding the registry!
+    public static final DeferredItem<Item> COPPER_NUGGET = ITEMS.register("copper_nugget",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+    //Method to execute the registry in the MAIN
+}
